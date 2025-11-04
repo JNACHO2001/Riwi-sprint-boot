ce18ad59d0edbd26a70443ade7f38500f710a198
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-11-03 20:20:14 -0500

    feat:terminacion de los controladores

---
+++ src/main/java/eventos/catalogos/controlador/UsuarioControlador.java
@@ -0,0 +1,63 @@
+package eventos.catalogos.controlador;
+
+import eventos.catalogos.service.UsuarioServiceIm;
+import eventos.catalogos.web.dto.UsuarioRequest;
+import eventos.catalogos.web.dto.UsuarioResponse;
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioControlador {
+
+    private final UsuarioServiceIm service;
+
+    public UsuarioControlador(UsuarioServiceIm service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> crear(@Valid @RequestBody UsuarioRequest req) {
+        var res = service.crear(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponse>> listar() {
+        return ResponseEntity.ok(service.listar());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> obtener(@PathVariable Integer id) {
+
+        return ResponseEntity.ok(service.obtenerPorId(id));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable Integer id) {
+        service.eliminar(id);
+        return ResponseEntity.ok("Eliminado");
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> editar(@PathVariable Integer id, @RequestBody UsuarioRequest req) {
+
+        var res = service.editar(id, req);
+
+        return ResponseEntity.ok().body(res);
+
+    }
+
+}
