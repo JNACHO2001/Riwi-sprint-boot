ec77129c036fd24dd0e6a4ee76b7d10e5371c02d
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-11-03 20:07:46 -0500

    feat:se crearon  los dto con las validaciones

---
+++ src/main/java/eventos/catalogos/web/dto/EventResponse.java
@@ -0,0 +1,55 @@
+package eventos.catalogos.web.dto;
+import java.time.LocalDate;
+
+public class EventResponse {
+    
+       private String nombre;
+    private LocalDate fechaEvento;
+     private String ubicacion;
+     private String ciudad;
+
+    public EventResponse(String nombre, LocalDate fechaEvento, String ubicacion, String ciudad) {
+        this.nombre = nombre;
+        this.fechaEvento = fechaEvento;
+        this.ubicacion = ubicacion;
+        this.ciudad = ciudad;
+    }
+
+  
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaEvento() {
+        return fechaEvento;
+    }
+
+    public void setFechaEvento(LocalDate fechaEvento) {
+        this.fechaEvento = fechaEvento;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    
+    
+    
+
+}
