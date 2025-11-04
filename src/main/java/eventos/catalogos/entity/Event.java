e1053fe2bac7713fb48bd1f204cdcbdf06ff2c25
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-11-03 19:48:28 -0500

    feat:se crearon las entidades y sus relaciones

---
+++ src/main/java/eventos/catalogos/entity/Event.java
@@ -0,0 +1,94 @@
+package eventos.catalogos.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private LocalDate fechaEvento;
+     private String ubicacion;
+     private String ciudad;
+     
+     @OneToMany(mappedBy ="event",cascade = CascadeType.ALL,orphanRemoval = true )
+     private List<Inscripcion>inscripciones = new ArrayList<>();
+
+    public Event() {
+    }
+
+    public Event( String nombre, LocalDate fechaEvento, String ubicacion, String ciudad) {
+        this.nombre = nombre;
+        this.fechaEvento = fechaEvento;
+        this.ubicacion = ubicacion;
+        this.ciudad = ciudad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaEvento() {
+        return fechaEvento;
+    }
+
+    public void setFechaEvento(LocalDate fechaEvento) {
+        this.fechaEvento = fechaEvento;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public List<Inscripcion> getInscripciones() {
+        return inscripciones;
+    }
+
+    public void setInscripciones(List<Inscripcion> inscripciones) {
+        this.inscripciones = inscripciones;
+    }
+    
+    
+
+
+
+
+     
+     
+    
+
+}
