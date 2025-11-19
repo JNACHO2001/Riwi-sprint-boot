6855c968661aafee85ee2e5983857801894231e7
jose <joseesapel@gmail.com>
2025-11-18 19:53:32 -0500

    feat:implementacion de los casos de uso

---
+++ src/main/java/eventos/catalogos/aplication/usercases/CreatedUseCaseImpl.java
@@ -0,0 +1,14 @@
+package eventos.catalogos.aplication.usercases;
+
+import eventos.catalogos.domain.model.Task;
+import eventos.catalogos.domain.ports.in.TaskUseCase;
+
+public class CreatedUseCaseImpl  implements  TaskUseCase{
+
+    @Override
+    public Task createdTask(Task task) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+
+}
