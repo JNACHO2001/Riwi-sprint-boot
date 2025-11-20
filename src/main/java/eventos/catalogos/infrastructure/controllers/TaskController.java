1ec58fbf063368c6ba0d9c57eda7f69887fbff6a
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-11-19 21:06:29 -0500

    fix:creacion del controlador

---
+++ src/main/java/eventos/catalogos/infrastructure/controllers/TaskController.java
@@ -0,0 +1,33 @@
+
+package eventos.catalogos.infrastructure.controllers;
+
+import eventos.catalogos.aplication.services.TaskService;
+import eventos.catalogos.domain.model.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createdTask(task);
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    }
+    
+    
+    
+    
+    
+    
+}
